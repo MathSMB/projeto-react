974b9e32e3e8b152000f12be2a1f5cff0d7c5c84
Matheus Breder <matheusbrederpuc@gmail.com>
2025-09-17 21:12:52 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HelloWorld from './components/HelloWorld';
+import HelloNewRoute from './components/HelloNewRoute';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HelloWorld />} />
+          <Route path="/my-new-route" element={<HelloNewRoute />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/HelloNewRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HelloNewRoute = () => {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontSize: '2rem',
+      fontFamily: 'Arial, sans-serif',
+      color: '#0066cc'
+    }}>
+      <h1>Hello New Route</h1>
+    </div>
+  );
+};
+
+export default HelloNewRoute;
